400dbdea65a1f1498527277714d94c15b64dd4f3
Bjurman Joel <cyramica@users.noreply.github.com>
2017-04-27 11:22:40 +0200

    Semi-working

--- eveapi.js
+++ eveapi.js
@@ -1,10 +1,19 @@
-function getItemName(name) {
+function get_item_name(items) {
 
     var xhrItemName = new XMLHttpRequest();
-    xhrItemName.open("GET", "https://api.eveonline.com/eve/TypeName.xml.aspx?ids=" + name , false);
+    var typeIDs = "";
+    for (i = 0; i < items.length; i++) {
+        typeIDs += "&ids=" + items[i]
+        console.log('get_item_name typeIDs: ' + typeIDs);
+    }
+    xhrItemName.open("GET", "https://api.eveonline.com/eve/TypeName.xml.aspx?" + typeIDs , false);
     xhrItemName.send();
    
- //   xmlItemName = xhrItemName.responseXML;
- //   var itemData = xmlItemName.getElementsByTagName("row");
- //   return itemData[0].getAttribute('typeName');
+    var xmlItemName = xhrItemName.responseXML;
+    var itemData = xmlItemName.getElementsByTagName("row");
+    var names = [];
+    for (i = 0; i < itemData.length; i++) {
+        names[i] = itemData[i].getAttribute('typeName');
+    }
+    return names;
 }
